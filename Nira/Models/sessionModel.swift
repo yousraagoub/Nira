1a5fe5286b2ee5494f5065eaf9cbc46366948657
yousraagoub <yousraagoub@gmail.com>
2025-12-10 09:08:21 +0300

    feat: add insight view and session model.

--- Nira/Models/sessionModel.swift
+++ Nira/Models/sessionModel.swift
@@ -4,4 +4,10 @@
 //
 //  Created by Yousra Abdelrahman on 11/06/1447 AH.
 //
+import Foundation
+struct sessionData: Identifiable {
+    let id = UUID()
+    let day: String
+    let value: Double
+}
 
